9c5fbd198eeec2026726968d6bb65af16ce154a9
Heitor Silvano <heitor.nunes182@hotmail.com>
2026-05-03 18:23:12 -0300

    chore: validation pipe to parse requisitions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000
-  await app.listen(port);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(port);
   console.log('Server running at port', port)
 }
 bootstrap();
